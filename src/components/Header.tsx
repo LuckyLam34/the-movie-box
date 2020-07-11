a21a3280207b4cd293d8522caec44bcde10fd7b3
Lucky <lucky@siliconstack.com.au>
2020-07-11 18:13:22 +0700

    Added: (Header) search icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -6,25 +6,46 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
-export const Header = () => (
-  <div className="header">
-    <div className="container">
-      <Navbar expand="lg">
-        <Navbar.Brand href="#home">THEMOVIEBOX</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <div className="mr-auto"></div>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button className="text-uppercase" variant="link">
-              log in
-            </Button>
-            <Button className="text-uppercase" variant="primary">
-              sign up
-            </Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  </div>
-);
+export const Header = () => {
+  const [showIcon, setShowIcon] = useState(true);
+
+  return (
+    <section className="header">
+      <div className="container">
+        <Navbar expand="lg">
+          <Navbar.Brand href="#home">THEMOVIEBOX</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <div className="mr-auto"></div>
+            <form className="form-inline my-2 my-lg-0">
+              <img
+                onClick={() => {
+                  setShowIcon(false);
+                }}
+                className={showIcon ? 'show' : 'hide'}
+                src="/assets/images/search-icon.png"
+              />
+              <input
+                onBlur={() => {
+                  setShowIcon(true);
+                }}
+                className={
+                  (!showIcon ? 'show' : 'hide') + ' form-control mr-sm-2'
+                }
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-link text-uppercase" type="button">
+                log in
+              </button>
+              <button className="btn btn-primary text-uppercase" type="submit">
+                sign up
+              </button>
+            </form>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    </section>
+  );
+};
